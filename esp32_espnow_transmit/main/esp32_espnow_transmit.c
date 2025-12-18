d24dfacd5add19fe0d9dc57e60caa5f2c7495541
Alexander Bobkov <alexandre.bobkov@gmail.com>
2025-12-17 23:01:01 -0500

    espnow transmit

--- esp32_espnow_transmit/main/esp32_espnow_transmit.c
+++ esp32_espnow_transmit/main/esp32_espnow_transmit.c
@@ -1,13 +1,16 @@
 /*
+    Description:    ESPNOW transmitter node
+
     Author: Alexander Bobkov
 
     Date Created:   Dec 17, 2025
     Date Updated:   Dec 17, 2025
 
-    Compiled using esp-idf v
+    Compiled using esp-idf v5.4.1
 
     Updated:
 */
+
 #include <stdio.h>
 
 void app_main(void)
